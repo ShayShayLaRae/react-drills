c56991d36207f46bef1bccb12d3b707f9a295ba4
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-11-01 23:41:16 -0600

    i think i finished this one a while back too. just dont want to dig atm so pushing and checking out later

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,13 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Image from './Image';
 
 class App extends Component {
-  render() {
+  render(){
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <Image url={'https://404store.com/2017/12/08/random-pic-14.jpg'} />
       </div>
     );
   }
---
+++ app-8/src/axios.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+class axios extends Component {
+    constructor(){
+        super();
+
+    }
+    }
